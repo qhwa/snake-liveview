931a383563a2a903c7684721914c03c534c5f834
qhwa <qhwa@163.com>
2019-08-30 13:10:43 -0600

    init commit

---
+++ lib/snake_web/router.ex
@@ -0,0 +1,26 @@
+defmodule SnakeWeb.Router do
+  use SnakeWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", SnakeWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", SnakeWeb do
+  #   pipe_through :api
+  # end
+end
